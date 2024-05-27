bf35e50ec8af75f2e07c7b826f4c2985fdd8b820
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2024-05-28 00:22:27 +0300

    save

--- src/js/pages/registration.js
+++ src/js/pages/registration.js
@@ -1,27 +1,41 @@
-// "use strict";
-// import buttonBurger from "./modules/button-burger";
-// import order from "./modules/order";
-// import seeds from "./modules/seeds";
-// import navProd from "./modules/navProd";
-// import scrollToTop from "./modules/scrollToTop";
-// import validateName from "./modules/validateName";
-// import maskPhone from "./modules/maskPhone";
+"use strict";
+
+import order from '../../blocks/order/order';
+import buttonBurger from '../../blocks/button-burger/button-burger';
+import sliders from '../../blocks/slider/slider';
+import seeds from '../../blocks/seeds/seeds';
+import navProd from '../../blocks/nav-prod/navProd';
+import scrollToTop from '../../blocks/scrollToTop/scrollToTop';
+import modal from '../../blocks/modal/modal';
+import modalBasket from '../../blocks/modal-basket/modal-basket';
+import modalLogin from '../../blocks/modal-login/modalLogin';
+
+import maskPhone from "../../blocks/maskPhone/maskPhone";
+// import validateName from "../../blocks/registration-content";
 // import validatePassword from "./modules/validatePassword";
 // import validateAllFilds from "./modules/validateAllFilds";
 // import validateEmail from "./modules/validateMail";
 // import validatePasswordConfirm from "./modules/validatePasswordConfirm";
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     buttonBurger("dropdown-panel");
-//     order();
-//     seeds();
-//     navProd();
-//     scrollToTop();
-//     validateEmail("email");
-//     validateName("name");
-//     validateName("surname");
-//     validatePassword("password");
-//     maskPhone();
-//     validatePasswordConfirm("password", "confirm-password");
-//     validateAllFilds();
-// });
+document.addEventListener("DOMContentLoaded", () => {
+
+    order();
+    buttonBurger('dropdown-panel');
+    sliders();
+    seeds();
+    navProd();
+    scrollToTop();
+    modal('.modal-login', '.btn-open-modal', '.modal-login__btn-close');
+    modal('.modal-basket', '.btn-modal-basket', '.modal-basket__btn-close');
+    modalLogin();
+    modalBasket();
+    
+    //---
+    maskPhone();
+    validateEmail("email");
+    // validateName("name");
+    // validateName("surname");
+    // validatePassword("password");
+    // validatePasswordConfirm("password", "confirm-password");
+    // validateAllFilds();
+});
