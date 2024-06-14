ef4519579ad2caddf9a9a71ec65e78c6871dc953
Belokonenko <Belokonenko.Viacheslav@gmail.com>
2024-06-14 13:58:42 +0300

    fix: filter-button

--- src/blocks/filter/filter.js
+++ src/blocks/filter/filter.js
@@ -1,17 +1,20 @@
-export const filter = () =>  {
-    const filterBtn = document.querySelector('.catalog-prod__btn-filter');
-    const filterBlock = document.querySelector('.filter');
+export const filter = () => {
+
+            console.log('filter');
+    const filterBtn = document.querySelector('.catalog__btn-filter');
+    const filterBlock = document.querySelector('.filter__form');
     const filterBtnClose = document.querySelector('.filter__btn');
 
     if (filterBtn) {
         filterBtn.addEventListener('click', () => {
-            filterBlock.classList.toggle('filter_active');
+            filterBlock.classList.toggle('filter__form_active');
+            console.log('.catalog__btn-filter');
         });
     }
 
     if (filterBtnClose) {
         filterBtnClose.addEventListener('click', () => {
-            filterBlock.classList.remove('filter_active');
+            filterBlock.classList.remove('filter__form_active');
         });
     }
-}
+};
