705f39b08808f766cea0936fc1e76882f5820448
Belokonenko <Belokonenko.Viacheslav@gmail.com>
2024-07-07 10:03:39 +0300

    add : h2

--- src/blocks/make-an-order/make-an-order.js
+++ src/blocks/make-an-order/make-an-order.js
@@ -11,6 +11,40 @@ export const makeAnOrder = () => {
 	}
 	// ---
 
+	// 1 . input region 
+	// 	1. выподающий список 
+	// 	2. поиск по написаному
+	// 	3. авто установка если город выбран
+	// 	4. заглавная буква если нет
+	// 	5. проверка есть такой region
+	// 2 . input city
+	// 	1. выподающий список (из отобраных если region выбран)
+	// 	2. поиск по списку
+	// 	4. заглавная буква если нет
+	// 	5. проверка есть такой city
+	// 3 . input deportament
+	// 	1. выподающий список (из отобраных если city выбран)
+	// 	2. поиск по списку
+	// 	5. проверка есть такой deportament
+	// 4 . input index
 
+	// input region
+	//
+
+	const blockDelivery = document.querySelector('.make-an-order__delivery');
+
+	if (blockDelivery) {
+		blockDelivery.addEventListener('input', (e) => {
+			const target = e.target;
+			console.log(target.classList.contains("region"))
+
+			if (target && target.classList.contains("region")) {
+
+			}
+		})
+
+
+
+	}
 }
 
