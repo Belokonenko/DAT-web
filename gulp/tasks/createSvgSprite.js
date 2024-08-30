0b62f2aa7d2a87824c0b787b6888111222e4922c
Belokonenko <Belokonenko.Viacheslav@gmail.com>
2024-08-30 20:49:23 +0300

    Add : sprite.html

--- gulp/tasks/createSvgSprite.js
+++ gulp/tasks/createSvgSprite.js
@@ -1,25 +1,57 @@
+// import gulp from 'gulp';
+// import svgSprite from 'gulp-svg-sprite';
+//
+// import { filePaths } from '../config/paths.js';
+// import { logger } from '../config/Logger.js';
+//
+// const createSvgSprite = () => {
+//     return gulp
+//         .src(filePaths.src.svgIcons, {})
+//         .pipe(logger.handleError('COPY ROOT FILES'))
+//         .pipe(
+//             svgSprite({
+//                 mode: {
+//                     symbol: {
+//                         sprite: '../sprite.html',
+//
+//                         /** Создавать страницу с перечнем иконок */
+//                         example: true,
+//                     },
+//                 },
+//             })
+//         )
+//         .pipe(gulp.dest(filePaths.srcFolder + '/blocks/svg-sprites/'));
+// };
+//
+// export { createSvgSprite };
+
 import gulp from 'gulp';
 import svgSprite from 'gulp-svg-sprite';
+import newer from 'gulp-newer'; // Импортируем плагин 'gulp-newer'
 
 import { filePaths } from '../config/paths.js';
-import { logger } from "../config/Logger.js";
+import { logger } from '../config/Logger.js';
 
 const createSvgSprite = () => {
-  return gulp.src(filePaths.src.svgIcons, {})
-    .pipe(logger.handleError('COPY ROOT FILES'))
-    .pipe(
-      svgSprite({
-        mode: {
-          symbol: {
-            sprite: '../sprite.svg',
+    const destinationPath = filePaths.srcFolder + '/blocks/svg-sprites/'; // Определяем путь назначения для проверки 'newer'
 
-            /** Создавать страницу с перечнем иконок */
-            example: true,
-          },
-        },
-      })
-    )
-    .pipe(gulp.dest(filePaths.srcFolder + '/blocks/svg-sprites/'));
+    return gulp
+        .src(filePaths.src.svgIcons, {}) // Исходные SVG-иконки
+        .pipe(logger.handleError('COPY ROOT FILES')) // Обработка ошибок
+        .pipe(newer(destinationPath + 'sprite.html')) // Проверка наличия изменений с помощью 'newer'
+        .pipe(
+            svgSprite({
+                mode: {
+                    symbol: {
+                        sprite: '../sprite.html', // Путь к создаваемому спрайту
+
+                        /** Создавать страницу с перечнем иконок */
+                        example: true, // Создаём HTML-страницу с перечнем иконок
+                    },
+                },
+            })
+        )
+        .pipe(gulp.dest(destinationPath)); // Сохраняем спрайт и HTML-страницу
 };
 
 export { createSvgSprite };
