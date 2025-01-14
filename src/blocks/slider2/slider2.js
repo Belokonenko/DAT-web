1c63cd6122f971da9c8e228bfef2a44dd550fad7
d <d@dh.dn>
2025-01-14 12:40:08 +0200

    add : json data partners

---
+++ src/blocks/slider2/slider2.js
@@ -0,0 +1,629 @@
+export default function sliders2(nameSlider) {
+
+    const slider = document.querySelector(`[data-name_slider="${nameSlider}"]`);
+
+    if (!slider) return;
+
+    const API_URL = `http://localhost:4000/${nameSlider}`;
+    const sliderLine = slider.querySelector('.slider__line');
+
+    let counnt = 0; //current card number
+
+    main();
+
+    ///////////////////////////////////////////////////////////////////////
+
+    async function main() {
+        const dataCards = await getData(API_URL);
+
+        setWidthCard();
+
+        window.addEventListener('resize', debouncedResizeHandler);
+
+        await renderCards(dataCards, sliderLine);
+
+        createDots(slider);
+
+        const bntLeft = slider.querySelector('.slider__btn-left');
+        const bntRight = slider.querySelector('.slider__btn-right');
+
+        bntLeft.addEventListener('click', () => {
+            left();
+        });
+
+        bntRight.addEventListener('click', () => {
+            right();
+        });
+
+
+        console.log('exit main');
+    }
+
+    ///////////////////////////////////////////////////////////////////////
+
+    // --- Infinity---
+
+    function moveLineQ() {
+        sliderLine.style.transition = 'none';
+        sliderLine.style.transform = `translateX(-${getWidthShift() * counnt}px)`;
+    }
+
+    function addItemBeginn() {
+        console.log('addItemBeginn()')
+
+        const sliderLine = slider.querySelector('.slider__line');
+        const slides = Array.from(sliderLine.children);
+        const firstSlide = slides[0];
+        const lastSlide = slides[slides.length - 1];
+
+        sliderLine.insertBefore(lastSlide.cloneNode(true), firstSlide);
+
+        sliderLine.removeChild(sliderLine.lastElementChild);
+        ++counnt;
+        console.log(counnt)
+        moveLineQ();
+
+    }
+
+    function addItemEnd() {
+
+        console.log('addItemEnd()')
+
+        const sliderLine = slider.querySelector('.slider__line');
+        const slides = Array.from(sliderLine.children);
+        const firstSlide = slides[0];
+        const lastSlide = slides[slides.length - 1];
+
+        sliderLine.appendChild(firstSlide.cloneNode(true));
+        firstSlide.remove();
+        --counnt;
+        console.log(counnt)
+        moveLineQ();
+    }
+
+    // ---/Infinity---
+
+    function getCounntItems() {
+        return slider.querySelectorAll('.slider__item').length;
+    }
+
+    function getMaxCounnt() {
+        return getCounntItems() - getCounntVisebleItem();
+    }
+
+    // ---  swipe ---
+
+    let posInit = 0;
+    let posX1 = 0;
+    let posX2 = 0;
+    let posFinal = 0;
+
+    let trfRegExp = /[-0-9.]+(?=px)/;
+
+    slider.addEventListener('mousedown', swipeStart);
+    slider.addEventListener('touchstart', swipeStart);
+
+    function getEvent(event) {
+        // return evetn for touch or mouse to .clientX
+        return event.type.search('touch') !== -1 ? event.touches[0] : event;
+    }
+
+    function swipeStart(event) {
+        const sliderLine = slider.querySelector('.slider__line');
+
+        let e = getEvent(event);
+
+        posInit = posX1 = e.clientX; // first coordinate X axis
+
+        sliderLine.style.transition = ''; // remove the smooth transition
+
+        // отслеживать другие события на документе
+        slider.addEventListener('touchmove', swipeAction);
+        slider.addEventListener('touchend', swipeEnd);
+        slider.addEventListener('mousemove', swipeAction);
+        slider.addEventListener('mouseup', swipeEnd);
+        slider.addEventListener('mouseout', swipeEnd);
+        slider.addEventListener('pointerout', swipeEnd);
+    }
+
+    function swipeAction(event) {
+        const sliderLine = slider.querySelector('.slider__line');
+        let e = getEvent(event);
+        let style = sliderLine.style.transform || 'translateX(0px)';
+
+        console.log(style)
+
+        let transform = +style.match(trfRegExp)[0]; // считываем трансформацию с помощью регулярного выражения и сразу превращаем в число
+
+        posX2 = posX1 - e.clientX; // получаем разницу start и mouve
+
+        posX1 = e.clientX; // текущее положение в x1
+
+        sliderLine.style.transform = `translateX(${transform - posX2}px)`; // двигаем sliderLine
+    }
+
+    function swipeEnd() {
+        // финальная позиция курсора
+        posFinal = posInit - posX1;
+
+        slider.removeEventListener('touchmove', swipeAction);
+        slider.removeEventListener('mousemove', swipeAction);
+        slider.removeEventListener('touchend', swipeEnd);
+        slider.removeEventListener('mouseup', swipeEnd);
+        slider.removeEventListener('mouseout', swipeEnd);
+
+        slider.removeEventListener('pointerout', swipeEnd);
+
+        if (Math.abs(posFinal) > getMaxCounnt()) {
+            // убираем знак минус и сравниваем с порогом сдвига слайда
+            if (posInit < posX1) {
+                // если мы тянули вправо, то уменьшаем номер текущего слайда
+                --counnt; // если мы тянули влево, то увеличиваем номер текущего слайда
+            } else if (posInit > posX1) {
+                ++counnt;
+            }
+        }
+
+        // если курсор двигался, то запускаем функцию переключения слайдов
+        if (posInit !== posX1) {
+        }
+
+        moveLine(counnt);
+    }
+    // --- /swipe ---
+
+    function getWidthItem() {
+        const cell = slider.querySelector('.silder__cell');
+        return cell.getBoundingClientRect().width;
+    }
+
+    function getGap() {
+        const styleSlider = getComputedStyle(slider);
+        return styleSlider.getPropertyValue('--slider-gap').trim();
+    }
+
+    // --- move ---
+
+
+    function getCounntVisebleItem() {
+        const styleSlider = getComputedStyle(slider);
+        return styleSlider.getPropertyValue('--slider-count-card').trim();
+    }
+
+
+    function getWidthShift() {
+        return (getWidthItem() + parseInt(getGap()));
+    }
+
+    function moveLine(num) {
+        console.log(`moveLine(${num})`)
+        if (num < 0) {
+            num = getCounntItems();
+        }
+
+        if (num > getCounntItems() - getCounntVisebleItem()) {
+            num = 0;
+        }
+
+        const sliderLine = slider.querySelector('.slider__line');
+
+        sliderLine.style.transform = `translateX(-${getWidthShift() * num}px)`;
+        activeDot(num);
+        counnt = num;
+    }
+
+    function left() {
+        console.log('fun left() counnt = ', counnt)
+        moveLine(--counnt);
+    }
+
+    function right() {
+        console.log('fun right() counnt = ', counnt)
+        moveLine(++counnt);
+    }
+
+    // --- /move ---
+
+    // --- resize
+
+    function resetSize() {
+        console.log('resetSize()')
+        setWidthCard();
+        moveLine(counnt)
+        createDots(slider);
+    }
+
+    function debounce(func, delay) {
+        let timeoutId;
+
+        return function(...arg) {
+            clearTimeout(timeoutId);
+
+            timeoutId = setTimeout(() => {
+                func(...arg)
+            }, delay)
+        }
+    }
+
+    const debouncedResizeHandler = debounce(resetSize, 300);
+
+    // --- /resize
+
+    function setWidthCard() {
+        slider.style.setProperty('--width-card', `${getWidthItem()}px`);
+    }
+
+    async function getData(url) {
+        try {
+            const res = await fetch(url);
+            if (res.ok) {
+                const data = await res.json();
+                return data;
+            } else {
+                throw new Error(`Error : ${res.status}`);
+            }
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function renderCards(arrDataCards, parentBlock) {
+        parentBlock.innerHTML = "";
+
+        arrDataCards.forEach(data => {
+
+            const element = document.createElement('li');
+
+            element.classList.add('slider__item');
+
+            element.innerHTML = `
+
+            <li class="slider__item partners__item">
+                <picture>
+                    <source
+                        type="image/webp"
+                        srcset="
+                            @@path_img.webp    1x,
+                            @@path_img@2x.webp 2x,
+                            @@path_img@3x.webp 3x,
+                            @@path_img@4x.webp 4x
+                        "
+                    />
+                    <img
+                        class="@@class-name"
+                        width="@@width"
+                        height="@@height"
+                        src="@@path_img.png"
+                        srcset="
+                            @@path_img@2x.png 2x,
+                            @@path_img@3x.png 3x,
+                            @@path_img@4x.png 4x
+                        "
+                        alt="@@alt"
+                    />
+                </picture>
+            </li>
+            `;
+
+            parentBlock.appendChild(element);
+        });
+
+        const numEmptyCards = getCounntVisebleItem() - parentBlock.children.length;
+
+        if (numEmptyCards > 0) {
+            console.log(`parentBlock.children.length = ${parentBlock.children.length}`)
+            console.log(`getCounntVisebleItem() = ${getCounntVisebleItem()}`)
+            console.log(` = ${getCounntVisebleItem() - parentBlock.children.length}`)
+
+            for (let index = 0; index < numEmptyCards; index++) {
+                const element = document.createElement('li');
+
+                element.classList.add('slider__item');
+
+                element.innerHTML = `
+                <article class="card" draggable="false">
+                    </article>
+            `;
+
+                parentBlock.appendChild(element);
+
+            }
+        }
+    }
+
+    // --- dots ---
+
+    function createDots(slider) {
+        console.log('slider = ',slider);
+        const sliderItems = slider.querySelectorAll('.slider__item');
+        
+        console.log('sliderItems',sliderItems)
+        
+        const dotsList = slider.querySelector('.slider__dot-list');
+        const counntVisebleDots = sliderItems.length - (getCounntVisebleItem() - 1);
+
+        dotsList.innerHTML = ``;
+        console.log('counntVisebleDots = ', counntVisebleDots)
+
+        for (let index = 0; index < counntVisebleDots; index++) {
+            const dotWrap = document.createElement('li');
+            const dot = document.createElement('div');
+
+            dotWrap.classList.add('slider__dot-wrap');
+            dot.classList.add('slider__dot');
+
+            dotWrap.setAttribute('data-cunt', index);
+
+            dotWrap.addEventListener('click', () => {
+
+                if (counnt <= getCounntItems() - getCounntVisebleItem()) {
+                    counnt = index;
+                    moveLine(counnt);
+                    activeDot(counnt);
+                } else {
+                    counnt = index;
+                    activeDot(counnt);
+                }
+            });
+
+            dotsList.append(dotWrap);
+            dotWrap.append(dot);
+        }
+
+        activeDot(counnt);
+    }
+
+    function activeDot(num) {
+        const dots = slider.querySelectorAll('.slider__dot');
+
+        dots.forEach(item => {
+            item.classList.remove('slider__dot--active');
+        });
+
+        dots[+num].classList.add('slider__dot--active');
+    }
+
+    // --- /dots ---
+}
+
+
+
+
+
+//function getSizeCard() {
+//    const cell = document.querySelector('.silder__cell');
+//    const width = cell.getBoundingClientRect().width;
+//    return width
+//
+//}
+
+///////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+//const slider = sliderblock.querySelector('.slider__viewport');
+//const sliderLine = sliderblock.querySelector('.slider__line');
+//const sliderItems = sliderblock.querySelectorAll('.slider__item');
+//------------------------------------------------------------
+//const bntLeft = sliderblock.querySelector('.slider__btn-left');
+//const bntRight = sliderblock.querySelector('.slider__btn-right');
+//------------------------------------------------------------
+//const dotsList = sliderblock.querySelector('.slider__dot-list');
+//------------------------------------------------------------
+
+//const debouncedResizeHandler = debounce(resetSize, 300);
+//
+//let counnt = Math.round(getCounntItems() / 2);
+//
+//// --- main ---
+//
+////createDots();
+//
+////resetSize();
+//
+//window.addEventListener('resize', debouncedResizeHandler);
+//
+//bntLeft.addEventListener('click', () => {
+//    left();
+//});
+//
+//bntRight.addEventListener('click', () => {
+//    right();
+//});
+//
+//// --- main ---
+//
+//// --- functions ---
+//
+//
+//function getWidthSlider() {
+//    return slider.clientWidth;
+//}
+//
+//function getWidthItem() {
+//    return getWidthSlider() / getCounntVisebleItem();
+//}
+//
+//function getCounntItems() {
+//    return sliderItems.length;
+//}
+//
+//function getMaxCounnt() {
+//    return getCounntItems() - getCounntVisebleItem();
+//}
+//
+//function resetSize() {
+//    setWidthItems();
+//    moveLine(counnt);
+//}
+//
+//function setWidthItems() {
+//    const slideWidth = `${getWidthSlider() / getCounntVisebleItem() - calcGap()}px`;
+//
+//    sliderItems.forEach(item => {
+//        // mouve all slids and set size
+//        item.style.width = slideWidth;
+//    });
+//}
+//
+//function getGap() {
+//    return parseInt(window.getComputedStyle(sliderLine).gap);
+//}
+//
+//function calcGap() {
+//    let gap = parseInt(window.getComputedStyle(sliderLine).gap);
+//    return (gap * (getCounntVisebleItem() - 1)) / getCounntVisebleItem();
+//}
+//
+//function getCounntVisebleItem() {
+//    let counntVisebleItem = 1;
+//    let width = getWidthSlider();
+//
+//    switch (true) {
+//        case width >= 1440:
+//            counntVisebleItem = 4;
+//            break;
+//        case width >= 1024:
+//            counntVisebleItem = 4;
+//            break;
+//        case width >= 992:
+//            counntVisebleItem = 4;
+//            break;
+//        case width >= 768:
+//            counntVisebleItem = 3;
+//            break;
+//        case width >= 425:
+//            counntVisebleItem = 2;
+//            break;
+//        case width >= 375:
+//            counntVisebleItem = 1;
+//            break;
+//        case width >= 325:
+//            counntVisebleItem = 1;
+//            break;
+//    }
+//
+//    return counntVisebleItem;
+//}
+//
+//// --- mouve ---
+//
+//function moveLine(num) {
+//    //------- no break point mouve -----------
+//    if (num < 0) {
+//        num = getMaxCounnt();
+//    }
+//
+//    if (num > getMaxCounnt()) {
+//        num = 0;
+//    }
+//
+//    //------- /no break point mouve -----------
+//
+//    sliderLine.style.transform = `translateX(-${(getWidthItem() + getGap() / getCounntVisebleItem()) * num}px)`;
+//
+//    activeDot(num);
+//    counnt = num;
+//}
+//
+//function left() {
+//    moveLine(--counnt);
+//}
+//
+//function right() {
+//    moveLine(++counnt);
+//}
+//
+//// --- /mouve ---
+//
+//// ---  swipe ---
+//
+//let posInit = 0;
+//let posX1 = 0;
+//let posX2 = 0;
+//let posFinal = 0;
+//
+//let trfRegExp = /[-0-9.]+(?=px)/;
+//
+//slider.addEventListener('mousedown', swipeStart);
+//slider.addEventListener('touchstart', swipeStart);
+//
+//function getEvent(event) {
+//    // return evetn for touch or mouse to .clientX
+//    return event.type.search('touch') !== -1 ? event.touches[0] : event;
+//}
+//
+//function swipeStart(event) {
+//    let e = getEvent(event);
+//    posInit = posX1 = e.clientX; // first coordinate X axis
+//
+//    sliderLine.style.transition = ''; // remove the smooth transition
+//
+//    // отслеживать другие события на документе
+//    slider.addEventListener('touchmove', swipeAction);
+//    slider.addEventListener('touchend', swipeEnd);
+//    slider.addEventListener('mousemove', swipeAction);
+//    slider.addEventListener('mouseup', swipeEnd);
+//    slider.addEventListener('mouseout', swipeEnd);
+//    slider.addEventListener('pointerout', swipeEnd);
+//}
+//
+//function swipeAction(event) {
+//    let e = getEvent(event);
+//    let style = sliderLine.style.transform; // получаем занчение transform в формате 'translateX(0px)'
+//    let transform = +style.match(trfRegExp)[0]; // считываем трансформацию с помощью регулярного выражения и сразу превращаем в число
+//
+//    posX2 = posX1 - e.clientX; // получаем разницу start и mouve
+//
+//    posX1 = e.clientX; // текущее положение в x1
+//
+//    sliderLine.style.transform = `translateX(${transform - posX2}px)`; // двигаем sliderLine
+//}
+//
+//function swipeEnd() {
+//    // финальная позиция курсора
+//    posFinal = posInit - posX1;
+//
+//    slider.removeEventListener('touchmove', swipeAction);
+//    slider.removeEventListener('mousemove', swipeAction);
+//    slider.removeEventListener('touchend', swipeEnd);
+//    slider.removeEventListener('mouseup', swipeEnd);
+//    slider.removeEventListener('mouseout', swipeEnd);
+//
+//    slider.removeEventListener('pointerout', swipeEnd);
+//
+//    if (Math.abs(posFinal) > getMaxCounnt()) {
+//        // убираем знак минус и сравниваем с порогом сдвига слайда
+//        if (posInit < posX1) {
+//            // если мы тянули вправо, то уменьшаем номер текущего слайда
+//            --counnt; // если мы тянули влево, то увеличиваем номер текущего слайда
+//        } else if (posInit > posX1) {
+//            ++counnt;
+//        }
+//    }
+//
+//    // если курсор двигался, то запускаем функцию переключения слайдов
+//    if (posInit !== posX1) {
+//    }
+//
+//    moveLine(counnt);
+//}
+//
+//// --- /swipe ---
+//
+//
+//// --- /functions ---
+//
+//
+//
+//
+//function initSlider(sliderblock) {
+//
+//
+//}
