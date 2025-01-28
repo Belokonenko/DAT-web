e34f12db45ff84297a5f06ae96576ef982139b7a
d <d@dh.dn>
2025-01-28 02:49:25 +0200

    Add : send data form order callback

--- src/blocks/order-callback-modal/order-callback-modal.js
+++ src/blocks/order-callback-modal/order-callback-modal.js
@@ -1,39 +1,82 @@
-export default function orderCallBackModal() {
-
+export default await function orderCallBackModal() {
+  const url = 'http://localhost:4000/usersCalback1';
   const dialog = document.getElementById('callbackDialog');
   const openBtns = document.querySelectorAll('.callback-btn');
 
   const closeBtn = document.querySelector('.close-btn');
-  const form  = document.getElementById('callbackForm');
-  console.log(form)
+  const form = document.getElementById('callbackForm');
+  const responseMessage = document.querySelector('.responseMessage');
 
-  // Открыть модальное окно
+  // open dialog window
   openBtns.forEach(openBtn => {
     openBtn.addEventListener('click', () => {
       dialog.showModal(); // Открыть диалог
     });
   })
 
-  // Закрыть модальное окно
+  // close dialog window
   closeBtn.addEventListener('click', () => {
     dialog.close(); // Закрыть диалог
+    form.classList.remove('hide')
   });
 
   // sent form
+
   form.addEventListener('submit', (event) => {
-    event.preventDefault();  
+    event.preventDefault();
     console.log('submit')
 
     const name = form.name.value.trim();
     const phone = form.phone.value.trim();
 
     if (name && phone) {
-      alert(`Thank you, ${name}! We will call you back at ${phone}.`);
-      dialog.close();
-      form.reset();
+
+      const formData = new FormData(form);
+      const jsonData = Object.fromEntries(formData.entries());
+      jsonData.timestamp = new Date().toISOString(); // YYYY-MM-DDTHH:mm:ss.sssZ
+      sendFormCallback(jsonData);
+
+      //form.classList.add('hide')
+      //alert(`Thank you, ${name}! We will call you back at ${phone}.`);
+      //dialog.close();
+      //form.reset();
     } else {
       alert('Please fill in all fields.');
     }
   });
+
+
+  async function sendFormCallback(jsonData) {
+    try {
+      
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(jsonData),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Server error: ${response.status}`);
+      }else {
+
+      const result = await response.json();
+      
+      form.reset();
+      form.classList.add('hide');
+      responseMessage.classList.remove('hide');
+      responseMessage.textContent = `Our operator will call you within 15 minutes. Thank you` ;
+      responseMessage.style.color = 'green';
+      
+      
+      }
+
+
+    } catch (error) {
+      responseMessage.textContent = `Submission failed. Please try again or call us.`;
+      responseMessage.style.color = 'red';
+    }
+  }
 };
 
